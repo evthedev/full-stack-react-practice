6c60cd92a0d5b1bdf66cf36c411492e32b29b71a
Ev Ooi <hello@evthedev.com>
2017-10-03 23:56:41 +0800

    Imported keys, created passport strategy, route handler

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require ('express');
 const passport = require ('passport');
 const GoogleStrategy = require ('passport-google-oauth20');
+const keys = require ('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
+
+// set up route handler for '/auth/google
+app.get(
+    '/auth/google', // the route
+    passport.authenticate( // Second argument tells the app to use passport.authenticate
+        'google', //first argument is the strategy called ‘google’
+        { scope: ['profile', 'email'] } // scope, or properties requested for permission
+    )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
